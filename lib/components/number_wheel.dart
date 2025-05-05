8b47446e64a4f7bdb2f3a3ca16aa04585588069e
Miguel <ningenmr@gmail.com>
2025-05-05 19:03:04 -0400

    App version beta

---
+++ lib/components/number_wheel.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Widget numberWheel({required Function(int) onSelectedItemChanged}){
+  final List<int> numbers = List.generate(100, (index) => index);
+  return SizedBox(
+    height: 150,
+    width: 100,
+    child: ListWheelScrollView.useDelegate(
+      itemExtent: 40,
+      perspective: 0.005,
+      diameterRatio: 1.2,
+      onSelectedItemChanged: onSelectedItemChanged,
+      physics: FixedExtentScrollPhysics(), 
+      childDelegate: ListWheelChildBuilderDelegate(
+        builder: (context, index) => Center(
+          child: Text(
+            numbers[index].toString(),
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        childCount: numbers.length,
+        ),
+      ),
+  );
+}
--- lib/screens/number_spinner_screen.dart
+++ lib/screens/number_spinner_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:number_spinner_app/components/number_wheel.dart';
 
 class NumberSpinnerScreen extends StatefulWidget {
   const NumberSpinnerScreen({super.key});
@@ -8,8 +9,42 @@ class NumberSpinnerScreen extends StatefulWidget {
 }
 
 class _NumberSpinnerScreenState extends State<NumberSpinnerScreen> {
+  int selectedNumber1 = 0;
+  int selectedNumber2 = 0;
+
+  final List<int> numbers = List.generate(100, (index) => index);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Sumador de Ruleta')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Selecciona dos numeros:', style: TextStyle(fontSize: 18)),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              numberWheel(
+                onSelectedItemChanged: (index){
+                  setState(() => selectedNumber1 = numbers[index]);
+                },
+              ),
+              numberWheel(
+                onSelectedItemChanged: (index){
+                  setState(() => selectedNumber2 = numbers[index]);
+                },
+              ),
+            ],
+          ),
+          SizedBox(height: 30),
+          Text(
+            'Resultado: ${selectedNumber1 + selectedNumber2}',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.cyanAccent),
+          )
+        ],
+      ),
+    );
   }
 }
