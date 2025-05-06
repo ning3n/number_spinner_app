824b501dae6c66f34750dd779cfcf387b083f215
Miguel <ningenmr@gmail.com>
2025-05-06 19:00:56 -0400

    Added reset button and corrected some styling on text and app colors to make it look more cyberpunk style

--- lib/components/number_wheel.dart
+++ lib/components/number_wheel.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-Widget numberWheel({required Function(int) onSelectedItemChanged}){
+Widget numberWheel({required FixedExtentScrollController controller, required Function(int) onSelectedItemChanged}){
   final List<int> numbers = List.generate(100, (index) => index);
   return SizedBox(
     height: 150,
     width: 100,
     child: ListWheelScrollView.useDelegate(
+      controller: controller,
       itemExtent: 40,
       perspective: 0.005,
       diameterRatio: 1.2,
@@ -15,7 +16,7 @@ Widget numberWheel({required Function(int) onSelectedItemChanged}){
         builder: (context, index) => Center(
           child: Text(
             numbers[index].toString(),
-            style: TextStyle(fontSize: 40),
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           ),
         ),
         childCount: numbers.length,
--- lib/screens/number_spinner_screen.dart
+++ lib/screens/number_spinner_screen.dart
@@ -13,25 +13,45 @@ class _NumberSpinnerScreenState extends State<NumberSpinnerScreen> {
   int selectedNumber2 = 0;
 
   final List<int> numbers = List.generate(100, (index) => index);
+  final FixedExtentScrollController controller1 = FixedExtentScrollController();
+  final FixedExtentScrollController controller2 = FixedExtentScrollController();
+
+  void resetear() {
+    controller1.jumpToItem(0);
+    controller2.jumpToItem(0);
+    setState(() {
+      selectedNumber1 = 0;
+      selectedNumber2 = 0;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller1.dispose();
+    controller2.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Sumador de Ruleta')),
+      appBar: AppBar(title: Text('Sumador de Ruleta', style: TextStyle(fontWeight: FontWeight.bold)), backgroundColor: Colors.pinkAccent, foregroundColor: Colors.white),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Selecciona dos numeros:', style: TextStyle(fontSize: 28)),
-          SizedBox(height: 20),
+          Text('Selecciona 2 numeros', style: TextStyle(fontSize: 30, color: Colors.cyan, fontWeight: FontWeight.bold),),
+          
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               numberWheel(
+                controller: controller1,
                 onSelectedItemChanged: (index){
                   setState(() => selectedNumber1 = numbers[index]);
                 },
               ),
               numberWheel(
+                controller: controller2,
                 onSelectedItemChanged: (index){
                   setState(() => selectedNumber2 = numbers[index]);
                 },
@@ -42,6 +62,21 @@ class _NumberSpinnerScreenState extends State<NumberSpinnerScreen> {
           Text(
             'Resultado: ${selectedNumber1 + selectedNumber2}',
             style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.cyanAccent),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 100, 
+              width: 100, 
+              child: ElevatedButton(
+                onPressed: resetear,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pinkAccent,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text('RESET', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15,))
+              )
+            ),
           )
         ],
       ),
